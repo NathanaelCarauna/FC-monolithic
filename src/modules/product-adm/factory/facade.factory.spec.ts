a77bc6ec6d92dae99b7e7bd8c8fb73ea40d16573
NathanaelCarauna <Nathanael.carauna@gmail.com>
2024-04-29 09:45:59 -0300

    feat: finish product facade with add and check methods

--- src/modules/product-adm/factory/facade.factory.spec.ts
+++ src/modules/product-adm/factory/facade.factory.spec.ts
@@ -43,4 +43,25 @@ describe("Facade factory test", () => {
       expect(productDb.purchasePrice).toEqual(input.purchasePrice);
       expect(productDb.stock).toEqual(input.stock);
     });
+
+    it("Should check product stock", async () => {
+      const productFacade = ProductAdmFacadeFactory.create();
+      const product = {
+        id: "1",
+        name: "Product 1",
+        description: "Product 1 description",
+        purchasePrice: 10,
+        stock: 10,
+      };
+
+      productFacade.addProduct(product);
+
+      const input = {
+        productId: "1"
+      }
+      const result = await productFacade.checkStock(input);
+
+      expect(result.productId).toBe(product.id);
+      expect(result.stock).toBe(product.stock);
+    })
 })
