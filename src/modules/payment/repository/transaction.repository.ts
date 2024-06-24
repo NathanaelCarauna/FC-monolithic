242fda1e70ca46db4e2e2662490506d75f4a6db4
NathanaelCarauna <Nathanael.carauna@gmail.com>
2024-06-24 08:07:11 -0300

    feat: implement transcation repository

---
+++ src/modules/payment/repository/transaction.repository.ts
@@ -0,0 +1,19 @@
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Transaction from "../domain/transaction";
+import PaymentGateway from "../gateway/payment.gateway";
+import TransactionModel from "./transaction.model";
+
+export default class TranscationRepository implements PaymentGateway{
+    async save(input: Transaction): Promise<Transaction> {
+        await TransactionModel.create({
+            id: input.id.id,
+            orderId: input.orderId,
+            amount: input.amount,
+            status: input.status,
+            createdAt: input.createdAt,
+            updatedAt: input.updatedAt,
+        })
+        return input
+    }
+
+}
---
+++ src/modules/payment/repository/transcation.repository.spec.ts
@@ -0,0 +1,45 @@
+import { Sequelize } from "sequelize-typescript";
+import TransactionModel from "./transaction.model";
+import Transaction from "../domain/transaction";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import TranscationRepository from "./transaction.repository";
+
+describe("Transaction Repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+    await sequelize.addModels([TransactionModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("Should save a transaction", async () => {
+    const transaction = new Transaction({
+        id: new Id("1"),
+        amount: 100,
+        orderId: "1",
+    })
+    transaction.approve();
+    const repository = new TranscationRepository();
+    const result =  await repository.save(transaction);
+
+    
+
+    expect(result.id.id).toBe(transaction.id.id)
+    expect(result.amount).toBe(transaction.amount)
+    expect(result.orderId).toBe(transaction.orderId)
+    expect(result.status).toBe("approved")
+    expect(result.createdAt).toBeDefined()
+    expect(result.updatedAt).toBeDefined()
+
+  });
+});
